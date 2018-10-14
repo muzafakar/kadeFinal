7490f59f8ca9f3e84c78ed107609eee4e9a4e371
Zulfakar Muhammad <muhammad.zulfakar@students.amikom.ac.id>
2018-10-15 00:26:06 +0700

    UI 70%

---
+++ app/src/main/java/com/muzadev/footballapp/fragment/MatchNextFragment.kt
@@ -0,0 +1,24 @@
+package com.muzadev.footballapp.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.muzadev.footballapp.R
+
+/**
+ * Created by zulfakar on 13/10/18.
+ * For educational purposes
+ */
+class MatchNextFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_spinner_list, container, false)
+        return view
+    }
+
+}
